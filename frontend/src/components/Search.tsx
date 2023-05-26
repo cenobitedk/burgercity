6c03e3726a6e32e5e3e640f4c97f40e7238730f3
Jeppe Hasseriis <jh@DESKTOP-LV0JNTP.localdomain>
2023-05-26 22:21:48 +0200

    Search request goes to BE

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./Search.css";
+import axios from "axios";
+import { environment } from "../environment";
 
 export default function Search() {
   const [term, setTerm] = useState("");
 
+  const handleSearch = useCallback((term: string) => {
+    axios
+      .get(`${environment.BACKEND_URL}/search?q=${term}`)
+      .then((response) => {
+        console.debug(response);
+      });
+  }, []);
+
   return (
-    <>
-      Search: <input type="search" className="searchbar" />{" "}
-    </>
+    <div className="searchbox">
+      Search:
+      <input
+        type="search"
+        className="searchbar"
+        onChange={(event) => {
+          setTerm(event.currentTarget.value);
+        }}
+        onKeyUp={(event) => {
+          if (event.key === "Enter") {
+            handleSearch(term);
+          }
+        }}
+      />
+    </div>
   );
 }
---
+++ frontend/src/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+};
