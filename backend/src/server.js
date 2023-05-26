8a33673ec1c47689b107a44dbc382fa91cd0735e
Jeppe Hasseriis <jh@DESKTOP-LV0JNTP.localdomain>
2023-05-26 22:19:03 +0200

    add CORS middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.get("/healthcheck", (req, res) => {
   res.send("OK");
 });
